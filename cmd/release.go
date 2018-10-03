442685ffbc30fd416311a616edea102f7043d882
Ian Auld <imauld@gmail.com>
2018-10-02 20:53:30 -0700

    Stub relese command, add conf file structs

--- cmd/release.go
+++ cmd/release.go
@@ -15,22 +15,60 @@
 package cmd
 
 import (
-	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
+	"github.com/kylie-a/waypoint/waypoint"
 	"github.com/spf13/cobra"
 )
 
 var (
-	appName, releaseType, target string
+	target string
 )
 
-func printError()     {}
-func printWarning()   {}
-func checkError()     {}
-func getVersion()     {}
-func buildImage()     {}
-func cleanup()        {}
-func packageChart()   {}
-func updateHelmRepo() {}
+type Release struct {
+	conf        *waypoint.Config
+	deploy      waypoint.Deployment
+	typ         waypoint.ReleaseType
+	prevVersion *waypoint.Version
+	newVersion  *waypoint.Version
+	docker      *client.Client
+}
+
+func NewRelease(conf *waypoint.Config, target string, typ waypoint.ReleaseType) Release {
+	var newVer *waypoint.Version
+
+	deploy := conf.GetDeployment(target)
+	prevVer, err := db.GetMostRecent(deploy.App)
+	checkErr(err, true, false)
+	if typ != waypoint.Rebuild {
+		newVer = bumpVersion(deploy.App, *prevVer, typ)
+	} else {
+		newVer = prevVer
+	}
+	cli, err := client.NewEnvClient()
+	checkErr(err, true, false)
+	return Release{
+		conf:        conf,
+		deploy:      deploy,
+		typ:         typ,
+		prevVersion: prevVer,
+		newVersion:  newVer,
+		docker:      cli,
+	}
+}
+
+func (r Release) Do() {
+	for _, step := range steps {
+		step.Execute(r)
+	}
+}
+
+func (r Release) getDockerListOpts(image string) types.ImageListOptions {
+	a := filters.NewArgs()
+	a.Add("reference", image)
+	return types.ImageListOptions{Filters: a}
+}
 
 // deployCmd represents the deploy command
 var releaseCmd = &cobra.Command{
@@ -43,18 +81,21 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		app := args[0]
-		prevVer, err := db.GetMostRecent(app)
-		checkErr(err)
 		releaseType := getReleaseType(cmd)
-		newVer := bumpVersion(appName, *prevVer, releaseType)
-		fmt.Println(newVer)
+		release := NewRelease(conf, target, releaseType)
+		release.Do()
+		//release.cleanup()
+		//release.buildImage()
+		//packageChart()
+		//updateHelmRepo()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(releaseCmd)
-	releaseCmd.Flags().StringVar(&appName, "app", "", "Application to release")
-	releaseCmd.Flags().StringVar(&releaseType, "type", "", "Release type")
+	releaseCmd.Flags().Bool("major", false, "Bump the major version up by one")
+	releaseCmd.Flags().Bool("minor", false, "Bump the minor version up by one")
+	releaseCmd.Flags().Bool("patch", false, "Bump the patch version up by one")
+	releaseCmd.Flags().Bool("rebuild", false, "Reuse the latest version up by one")
 	releaseCmd.Flags().StringVar(&target, "target", "", "Target env")
 }
--- waypoint/input.go
+++ waypoint/input.go
@@ -1,10 +1,16 @@
 package waypoint
 
 import (
-	"io/ioutil"
-	"gopkg.in/yaml.v2"
+	"encoding/base64"
 	"fmt"
+	"github.com/docker/docker-credential-helpers/client"
+	"github.com/docker/docker/pkg/archive"
+	"github.com/mitchellh/go-homedir"
 	"google.golang.org/api/option"
+	"gopkg.in/yaml.v2"
+	"io"
+	"io/ioutil"
+	"os"
 )
 
 type AuthKind string
@@ -12,19 +18,85 @@ type AuthKind string
 const (
 	ApiKey    AuthKind = "apiKey"
 	CredsFile AuthKind = "credsFile"
+	chartsAPI          = "/api/charts"
 )
 
+type HelmConf struct {
+	Name string `json:"name" yaml:"name"`
+}
+
+type DockerConf struct {
+	Repo    string `json:"repo" yaml:"repo"`
+	Context string `json:"context" yaml:"context"`
+	File    string `json:"file" yaml:"file"`
+}
+
+type Deployment struct {
+	App     string     `json:"app" yaml:"app"`
+	Project string     `json:"project" yaml:"project"`
+	Docker  DockerConf `json:"docker" yaml:"docker"`
+	Helm    HelmConf   `json:"helm" yaml:"helm"`
+}
+
+func (d Deployment) ImageName() string {
+	docker := d.Docker
+	return fmt.Sprintf("%s/%s", docker.Repo, d.App)
+}
+
+func (d Deployment) TaggedImageName(version string) string {
+	return fmt.Sprintf("%s:%s", d.ImageName(), version)
+}
+
+func (d Deployment) GetHelmURL(version string) string {
+	return fmt.Sprintf("%s%s/%s/%s", d.Helm.Name, chartsAPI, d.App, version)
+}
+
+func (d Deployment) GetDockerfile() string {
+	filePath, _ := homedir.Expand(d.Docker.File)
+	return filePath
+}
+
+func (d Deployment) GetContext() (io.Reader, error) {
+	ctxDir, err := homedir.Expand(d.Docker.Context)
+	if err != nil {
+		return nil, err
+	}
+
+	return archive.TarWithOptions(ctxDir, &archive.TarOptions{})
+}
+
+func (d Deployment) GetDockerAuth() string {
+	creds, err := client.Get(client.NewShellProgramFunc("docker-credential-gcloud"), "https://gcr.io")
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return base64.StdEncoding.EncodeToString([]byte(creds.Secret))
+}
+
+type Deployments map[string]Deployment
+
 type Auth struct {
-	Kind  AuthKind `json:"kind" yaml:"kind"`
-	Value string   `json:"value" yaml:"value"`
+	Kind    AuthKind `json:"kind" yaml:"kind"`
+	Project string   `json:"project" yaml:"project"`
+	Value   string   `json:"value" yaml:"value"`
 }
 
-type ConfigFile struct {
-	Project string `json:"project" yaml:"project"`
-	Auth    Auth   `json:"auth" yaml:"auth"`
+type Config struct {
+	Auth        Auth        `json:"auth" yaml:"auth"`
+	Deployments Deployments `json:"deployments" yaml:"deployments"`
 }
 
-func (c ConfigFile) GetAuth() option.ClientOption {
+func (c Config) GetDeployment(dep string) Deployment {
+	d, ok := c.Deployments[dep]
+	if !ok {
+		fmt.Printf("deploymetn %s not configured", dep)
+		os.Exit(2)
+	}
+	return d
+}
+
+func (c Config) GetAuth() option.ClientOption {
 	switch c.Auth.Kind {
 	case ApiKey:
 		return option.WithAPIKey(c.Auth.Value)
@@ -35,8 +107,9 @@ func (c ConfigFile) GetAuth() option.ClientOption {
 	}
 }
 
-func GetConf(fileName string) *ConfigFile {
-	conf := new(ConfigFile)
+func GetConf(fileName string) *Config {
+	conf := new(Config)
+	fileName, err := homedir.Expand(fileName)
 	b, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		fmt.Println(err.Error())
